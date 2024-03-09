e7d0beb7a921adf6d7458ac4983ed59ac7b8c4ec
Filip Maszota <filipmaszota3@gmail.com>
2024-03-09 10:14:14 +0100

    change components directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Hero from "./components/Hero/Hero";
+import Hero from "@/components/Hero/Hero";
 import Image from "next/image";
-import About from "./components/About";
+import About from "@/components/About";
 const page = () => {
   return (
     <main className="flex flex-col w-full">
